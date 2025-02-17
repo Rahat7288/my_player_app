efa9d9f57ae71eea71d86b7487df8eee6c7a8630
Rahat Islam Akash <rahatislamakash@gmail.com>
2025-02-17 08:57:58 +0600

    project constants are added

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,9 @@
+// utils/constants.dart
+const String METHOD_CHANNEL = 'com.example.media_player';
+const String PLAY_EVENT = 'play';
+const String PAUSE_EVENT = 'pause';
+const String STOP_EVENT = 'stop';
+const String COMPLETED_EVENT = 'completed';
+
+const String SAMPLE_AUDIO_URL = 'https://example.com/sample_audio.mp3';
+const String SAMPLE_VIDEO_URL = 'https://example.com/sample_video.mp4';
